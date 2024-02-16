67f3519a529a1dbf1e2afb73dd51b52bfb25d16e
Pedro Batista <130232046+phice421@users.noreply.github.com>
2024-02-15 21:03:48 -0300

    Add files via upload

--- teste.js
+++ teste.js
@@ -1,26 +1,28 @@
 var titulo = document.querySelector(".titulo-ganja");
 titulo.textContent = "Lista de Encomendas";
 
-document.querySelector(".total1").textContent
- = document.querySelector(".valor-unitario1").textContent
- * document.querySelector(".quantidade1").textContent
+document.querySelector(".total1").textContent =
+    document.querySelector(".valor-unitario1").textContent *
+    document.querySelector(".quantidade1").textContent;
 
- document.querySelector(".total2").textContent
- = document.querySelector(".valor-unitario2").textContent
- * document.querySelector(".quantidade2").textContent
+document.querySelector(".total2").textContent =
+    document.querySelector(".valor-unitario2").textContent *
+    document.querySelector(".quantidade2").textContent;
 
- document.querySelector(".total3").textContent
- = document.querySelector(".valor-unitario3").textContent
- * document.querySelector(".quantidade3").textContent
+document.querySelector(".total3").textContent =
+    document.querySelector(".valor-unitario3").textContent *
+    document.querySelector(".quantidade3").textContent;
 
- document.querySelector(".total4").textContent
- = document.querySelector(".valor-unitario4").textContent
- * document.querySelector(".quantidade4").textContent
+document.querySelector(".total4").textContent =
+    document.querySelector(".valor-unitario4").textContent *
+    document.querySelector(".quantidade4").textContent;
 
- document.querySelector(".total4").textContent
- = document.querySelector(".valor-unitario4").textContent
- * document.querySelector(".quantidade4").textContent
+document.querySelector(".total5").textContent =
+    document.querySelector(".valor-unitario5").textContent *
+    document.querySelector(".quantidade5").textContent;
 
- document.querySelector(".total5").textContent
- = document.querySelector(".valor-unitario5").textContent
- * document.querySelector(".quantidade5").textContent
+    // Função para formatar um número como R$
+function formatarParaReal(valor) {
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(valor);
+}
+
